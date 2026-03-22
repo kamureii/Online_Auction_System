315e4c77489f2f97f2246aeb6d818d6209b95138
kamurei <25023277@vnu.edu.vn>
2026-03-22 23:08:46 +0700

    Init project structure

--- src/main/java/com/auction/client/ClientMain.java
+++ src/main/java/com/auction/client/ClientMain.java
@@ -2,9 +2,11 @@ package com.auction.client;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class ClientMain {
-    static void main() {
+public class ClientMain
+{
+    static void main()
+    {
 
-        }
+
     }
 }
